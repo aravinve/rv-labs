f302bfe9dcc71416824440031ca026f23b066b66
Aravind Venkatesan <aravindtherocker@gmail.com>
2019-11-20 23:14:10 +0530

    Added Sections & Content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("RV Labs");
+$(document).ready(function() {
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 595) {
+        $('#navbar').addClass('navbar-fixed');
+      }
+      if ($(window).scrollTop() < 595) {
+        $('#navbar').removeClass('navbar-fixed');
+      }
+    });
+
+    $('a[class="nav-link d-flex"]').click(function(e){
+      e.preventDefault();
+      $('body, html').animate({
+        scrollTop: ($("#" + e.currentTarget.name).offset().top - 175)
+      }, 600);
+   });
+  });
+
+  
