9407039dd6d8ebfc5ac6cfc73f078a2e6be03a72
Aravind Venkatesan <aravindtherocker@gmail.com>
2019-12-01 14:07:54 +0530

    Add assets and products commit

--- js/main.js
+++ js/main.js
@@ -18,6 +18,53 @@ $(document).ready(function() {
         scrollTop: ($("#" + e.currentTarget.name).offset().top - 175)
       }, 600);
    });
+
+   $.getJSON("./assets/rv-products.json", function(json){
+     const herbaldata = json["rv-products"]["herbal"];
+     const aromadata = json["rv-products"]["aroma"];
+     const pharmaapidata = json["rv-products"]["pharmaapi"];
+     const researchdata = json["rv-products"]["research"];
+     const aminoacidsdata = json["rv-products"]["aminoacids"];
+
+     const herbaldatabody = $("#herbal-data-body")[0];
+     const aromadatabody = $("#aroma-data-body")[0];
+     const apidatabody = $("#api-table-body")[0];
+     const researchdatabody = $("#research-data-body")[0];
+     const aminoacidsdatabody = $("#aminoacids-data-body")[0];
+
+     generateSingleColTable(herbaldata, herbaldatabody);
+     generateSingleColTable(pharmaapidata, apidatabody);
+     generateDoubleColTable(aromadata, aromadatabody);
+     generateDoubleColTable(researchdata, researchdatabody);
+     generateDoubleColTable(aminoacidsdata, aminoacidsdatabody);
+   });
+
+   generateSingleColTable = (tableData, tableBodyElement) => {
+      for(var i=0; i< tableData.length;i++){
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const textNode1 = document.createTextNode(tableData[i]);
+        td1.appendChild(textNode1);
+        tr.appendChild(td1);
+        tableBodyElement.appendChild(tr);
+      }
+   }
+
+   generateDoubleColTable = (tableData, tableBodyElement) => {
+    for(var i=0; i< tableData.length;i++){
+      const tr = document.createElement("tr");
+          const td1 = document.createElement("td");
+          const td2 = document.createElement("td");
+          const textNode1 = document.createTextNode(tableData[i].name);
+          const textNode2 = document.createTextNode(tableData[i].casno);
+          td1.appendChild(textNode1);
+          td2.appendChild(textNode2);
+          tr.appendChild(td1);
+          tr.appendChild(td2);
+          tableBodyElement.appendChild(tr);
+    }
+ }
+
   });
 
   
